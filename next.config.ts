c3044ac6e1fa4856d7181d7f3783d371495018ad
sryuvaraj <sryuvaraj99@gmail.com>
2025-05-08 00:54:32 +0530

    dep

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // Enable static export
+  reactStrictMode: true,  // Optional: Enable React strict mode
 };
 
 export default nextConfig;
